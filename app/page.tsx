319763ab6831943a342e184c836c5e459e5b96db
ymc9 <104139426+ymc9@users.noreply.github.com>
2025-11-03 23:15:30 -0800

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,124 @@
-import Image from "next/image";
+'use client';
+
+import { schema } from '@/zenstack/schema-lite';
+import { useClientQueries } from '@zenstackhq/tanstack-query/react';
+import { LoremIpsum } from 'lorem-ipsum';
+import Image from 'next/image';
+
+const lorem = new LoremIpsum({ wordsPerSentence: { max: 6, min: 4 } });
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    const clientQueries = useClientQueries(schema);
+    const { data: users, isFetched: isUsersFetched } =
+        clientQueries.user.useFindMany();
+    const { data: posts } = clientQueries.post.useFindMany({
+        orderBy: { createdAt: 'desc' },
+        include: { author: true },
+    });
+    const createPost = clientQueries.post.useCreate();
+    const deletePost = clientQueries.post.useDelete();
+
+    const onCreatePost = () => {
+        if (!users) {
+            return;
+        }
+
+        // random title
+        const title = lorem.generateWords();
+
+        // random user as author
+        const forUser = users[Math.floor(Math.random() * users.length)];
+
+        console.log(
+            'Creating post for user:',
+            forUser.id,
+            'with title:',
+            title
+        );
+        createPost.mutate({
+            data: {
+                title,
+                authorId: forUser.id,
+            },
+        });
+    };
+
+    const onDeletePost = (postId: string) => {
+        deletePost.mutate({
+            where: { id: postId },
+        });
+    };
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+                <Image
+                    className="dark:invert"
+                    src="/next.svg"
+                    alt="Next.js logo"
+                    width={100}
+                    height={20}
+                    priority
+                />
+                <div className="flex flex-col mt-16 items-center gap-6 text-center sm:items-start sm:text-left">
+                    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                        My Awesome Blog
+                    </h1>
+
+                    {isUsersFetched ? (
+                        users && users.length > 0 ? (
+                            <button
+                                onClick={onCreatePost}
+                                className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 cursor-pointer"
+                            >
+                                New Post
+                            </button>
+                        ) : (
+                            <div className="p-4">
+                                No users found. Please run &quot;npm run
+                                db:init&quot; to seed the database.
+                            </div>
+                        )
+                    ) : (
+                        <p>Loading users ...</p>
+                    )}
+
+                    <div>
+                        <div>Current users</div>
+                        <div className="flex flex-col gap-1 p-2">
+                            {users?.map((user) => (
+                                <div
+                                    key={user.id}
+                                    className="text-sm text-gray-500"
+                                >
+                                    {user.email}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col container">
+                        {posts?.map((post) => (
+                            <div key={post.id} className="py-4">
+                                <div className="flex justify-between">
+                                    <h2 className="text-xl font-semibold">
+                                        {post.title}
+                                    </h2>
+                                    <button
+                                        className="ml-4 rounded-md px-2 py-1 text-white cursor-pointer underline text-xs"
+                                        onClick={() => onDeletePost(post.id)}
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                                <p className="text-sm text-gray-500">
+                                    by {post.author.name}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </main>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
