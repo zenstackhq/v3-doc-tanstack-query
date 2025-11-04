3be07f9361b627e0881c6ee2224cda5b53182ef6
ymc9 <yiming@whimslab.io>
2025-11-04 14:10:39 -0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,6 @@ function App() {
         // random user as author
         const forUser = users[Math.floor(Math.random() * users.length)];
 
-        console.log(
-            'Creating post for user:',
-            forUser.id,
-            'with title:',
-            title
-        );
         createPost.mutate({
             data: {
                 title,
@@ -46,61 +40,51 @@ function App() {
     };
 
     return (
-        <>
-            <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-                <main className="flex min-h-screen w-full max-w-3xl flex-col items-center p-4 bg-white dark:bg-black sm:items-start">
-                    <div className="flex flex-col mt-16 items-center gap-6 text-center sm:items-start sm:text-left">
-                        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                            My Awesome Blog
-                        </h1>
+        <main>
+            <h1>My Awesome Blog</h1>
 
-                        <button
-                            onClick={onCreatePost}
-                            className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 cursor-pointer"
+            <button
+                onClick={onCreatePost}
+                className="btn"
+            >
+                New Post
+            </button>
+
+            <div>
+                <div>Current users</div>
+                <div className="users">
+                    {users?.map((user) => (
+                        <div
+                            key={user.id}
+                            className="user"
                         >
-                            New Post
-                        </button>
-
-                        <div>
-                            <div>Current users</div>
-                            <div className="flex flex-col gap-1 p-2">
-                                {users?.map((user) => (
-                                    <div
-                                        key={user.id}
-                                        className="text-sm text-gray-500"
-                                    >
-                                        {user.email}
-                                    </div>
-                                ))}
-                            </div>
+                            {user.email}
                         </div>
+                    ))}
+                </div>
+            </div>
 
-                        <div className="flex flex-col container">
-                            {posts?.map((post) => (
-                                <div key={post.id} className="py-4">
-                                    <div className="flex justify-between">
-                                        <div className="flex flex-col text-left">
-                                            <h2 className="text-lg font-semibold">
-                                                {post.title}
-                                            </h2>
-                                            <p className="text-xs text-gray-500">by {post.author.name}</p>
-                                        </div>
-                                        <button
-                                            className="ml-4 rounded-md px-2 py-1 text-white cursor-pointer underline text-xs"
-                                            onClick={() =>
-                                                onDeletePost(post.id)
-                                            }
-                                        >
-                                            Delete
-                                        </button>
-                                    </div>
-                                </div>
-                            ))}
+            <div className="posts">
+                {posts?.map((post) => (
+                    <div key={post.id}>
+                        <div className="post">
+                            <div className="flex flex-col text-left">
+                                <h2>{post.title}</h2>
+                                <p className="author">by {post.author.name}</p>
+                            </div>
+                            <button
+                                className="btn-link"
+                                onClick={() =>
+                                    onDeletePost(post.id)
+                                }
+                            >
+                                Delete
+                            </button>
                         </div>
                     </div>
-                </main>
+                ))}
             </div>
-        </>
+        </main>
     );
 }
 
